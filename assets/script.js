639c64af69df4ae59c3cc8b005e81d2a66c45b9c
Rick Casillas <casillasrick@yahoo.com>
2022-05-15 01:36:16 -0400

    Added Alerts and Lists

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,32 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+
+function generatePassword() {
+    var userInput = window.prompt("How long would you like the password to be?")
+    var passwordLength = parseInt(userInput)
+
+    if (isNaN(passwordLength)) {
+        window.alert("Invalid")
+        return
+    }
+
+    if (passwordLength < 8 || passwordLength > 128) {
+        window.alert("Invalid")
+    }
+
+    var UserWantsNumbers = window.confirm("Include numbers?")
+    var UserWantsNumbers = window.confirm("Include symbols?")
+    var UserWantsNumbers = window.confirm("Include lowercase?")
+    var UserWantsNumbers = window.confirm("Include uppercase?")
+
+    var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+    var symbols = ["!", "@", "#", "$", "%"]
+    var lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+    var uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+
+}
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
