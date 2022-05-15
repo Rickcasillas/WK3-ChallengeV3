baf71a156af3dd070374eaaecbaf4539ecff948b
Rick Casillas <casillasrick@yahoo.com>
2022-05-15 13:22:41 -0400

    Stuck on password generator

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,19 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+function randomint(min, max) {
+    if (!max) {
+        max = min
+        min = 0
+    }
+  
+    var rand = Math.random()
+    return Math.floor(min*(1 - rand) + rand*max)
+}
+
+function getrandomitem(list) {
+    return list(randomint(0, options.length - 1))
+}
 
 function generatePassword() {
     var userInput = window.prompt("How long would you like the password to be?")
@@ -16,14 +29,42 @@ function generatePassword() {
     }
 
     var UserWantsNumbers = window.confirm("Include numbers?")
-    var UserWantsNumbers = window.confirm("Include symbols?")
-    var UserWantsNumbers = window.confirm("Include lowercase?")
-    var UserWantsNumbers = window.confirm("Include uppercase?")
+    var UserWantsSymbols = window.confirm("Include symbols?")
+    var UserWantsLowercase = window.confirm("Include lowercase?")
+    var UserWantsUppercase = window.confirm("Include uppercase?")
 
-    var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
-    var symbols = ["!", "@", "#", "$", "%"]
-    var lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-    var uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+    var numbersList = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+    var symbolsList = ["!", "@", "#", "$", "%", "^", "&", "*"]
+    var lowercaseList = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+    var uppercaseList = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+
+    var options = []
+
+    if (UserWantsNumbers === true) {
+        options.push(numbersList)
+    }
+
+    if (UserWantsSymbols === true) {
+        options.push(symbolsList)
+    }
+
+    if (UserWantsLowercase === true) {
+        options.push(lowercaseList)
+    }
+
+    if (UserWantsUppercase === true) {
+        options.push(uppercaseList)
+    }
+
+    var generatePassword = ""
+
+    for (var i = 0; 1 < passwordLength; i++) {
+        var randomlist = getrandomitem(options)
+        var randomchar = getrandomitem(randomlist)
+        generatedPassword += randomchar
+    }
+
+    return generatedPassword
 
 }
 
